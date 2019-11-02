e4343036e40ba366517f78d768b658cd301087ba
tolchielnikov <tolchielnikov@gmail.com>
2019-11-02 09:17:37 +0200

    test

--- SetDateTimeAtStart.py
+++ SetDateTimeAtStart.py
@@ -6,57 +6,88 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
-path = 'path'
+pth = r'\C$\ПУТЬ'
+sladh = r'\\'
 avtor = '©by Tolchelnikov'
-
-error_write = 'не поменял'
-error_none = 'файла с настройками нет'
-success = 'Успешно поменял'
-step = 20
-kass_list = []
+file_name = '\настройки'
+error_write = 'не поменял - '
+error_none = 'нет файла - '
+success_On = 'Успешно включил - '
+success_Off = 'Успешно выключил - '
+stepDown = 20
+stepRight = 350
+kass_list = ['1','2','3']
 root = Tk()
 root.title('SetDateTimeAtStart')
-canvas = Canvas(root, width=600, height=600)
+canvas = Canvas(root, width=700, height=700)
 canvas.pack()
-canvas.create_text(500, 580, text=avtor, fill='black', font=('Helvetica', 16))
+canvas.create_text(550, 680, text=avtor, fill='black', font=('Helvetica', 16))
 
 
 def setDateTimeAtStart_ON():
     canvas.delete("all")
-    canvas.create_text(500, 580, text=avtor, fill='black', font=('Helvetica', 16))
+    canvas.create_text(550, 680, text=avtor, fill='black', font=('Helvetica', 16))
+    i = 0
+    first_x = 100
+    first_z = 120
 
-    canvas.clipboard_clear()
-    if (os.path.exists(path)):
-        first_x = 200
-        config = configparser.ConfigParser()
-        try:
-            config.read(path)
-            config.set("Reliability", "setdatetimeatstart", "1")
-            with open(path, 'w') as configfile:  # save
-                config.write(configfile)
-            canvas.create_text(90, first_x, text=success, fill='green', font=('Helvetica', 12))
-        except:
-            canvas.create_text(90, first_x, text=error_write, fill='red', font=('Helvetica', 12))
-    else:
-        canvas.create_text(90, 200, text=error_none, fill='red', font=('Helvetica', 12))
+    while i < len(kass_list):
+
+        canvas.clipboard_clear()
+        if os.path.isfile(sladh + kass_list[i] + pth + file_name):
+
+            config = configparser.ConfigParser()
+            try:
+                config.read(sladh + kass_list[i] + pth + file_name)
+                config.set("Reliability", "setdatetimeatstart", "1")
+                with open(sladh + kass_list[i] + pth + file_name, 'w') as configfile:  # save
+                    config.write(configfile)
+                canvas.create_text(first_z, first_x, text=success_On + kass_list[i], fill='green',
+                                   font=('Helvetica', 12))
+            except:
+                canvas.create_text(first_z, first_x, text=error_write + kass_list[i], fill='red',
+                                   font=('Helvetica', 12))
+        else:
+            canvas.create_text(first_z, first_x, text=error_none + kass_list[i], fill='red', font=('Helvetica', 12))
+        print(sladh + kass_list[i] + pth + file_name)
+        i = i + 1
+        first_x = first_x + stepDown
+        if first_x >= 680:
+            first_z = first_z + stepRight
+            first_x = 100
 
 
 def setDateTimeAtStart_OFF():
     canvas.delete("all")
-    canvas.create_text(500, 580, text=avtor, fill='black', font=('Helvetica', 16))
-    if (os.path.exists(path)):
-        first_x = 200
-        config = configparser.ConfigParser()
-        try:
-            config.read(path)
-            config.set("Reliability", "setdatetimeatstart", "0")
-            with open(path, 'w') as configfile:  # save
-                config.write(configfile)
-            canvas.create_text(90, first_x, text=success, fill='green', font=('Helvetica', 12))
-        except:
-            canvas.create_text(90, first_x, text=error_write, fill='red', font=('Helvetica', 12))
-    else:
-        canvas.create_text(90, 200, text=error_none, fill='red', font=('Helvetica', 12))
+    canvas.create_text(550, 680, text=avtor, fill='black', font=('Helvetica', 16))
+    i = 0
+    first_x = 100
+    first_z = 120
+
+    while i < len(kass_list):
+
+        canvas.clipboard_clear()
+        if os.path.isfile(sladh + kass_list[i] + pth + file_name):
+
+            config = configparser.ConfigParser()
+            try:
+                config.read(sladh + kass_list[i] + pth + file_name)
+                config.set("Reliability", "setdatetimeatstart", "0")
+                with open(sladh + kass_list[i] + pth + file_name, 'w') as configfile:  # save
+                    config.write(configfile)
+                canvas.create_text(first_z, first_x, text=success_Off + kass_list[i], fill='green',
+                                   font=('Helvetica', 12))
+            except:
+                canvas.create_text(first_z, first_x, text=error_write + kass_list[i], fill='red',
+                                   font=('Helvetica', 12))
+        else:
+            canvas.create_text(first_z, first_x, text=error_none + kass_list[i], fill='red', font=('Helvetica', 12))
+
+        i = i + 1
+        first_x = first_x + stepDown
+        if first_x >= 680:
+            first_z = first_z + stepRight
+            first_x = 100
 
 
 but_On = Button(root, text='Выключить', width=10, height=2, command=lambda: setDateTimeAtStart_ON())
